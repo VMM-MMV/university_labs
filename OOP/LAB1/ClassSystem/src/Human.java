7b07d529cc9a0b33ec39e965d4e102ff7d39f5ba
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-11 18:43:24 +0300

    Changes in Parent Class

--- OOP/LAB1/ClassSystem/src/Human.java
+++ OOP/LAB1/ClassSystem/src/Human.java
@@ -1,13 +1,15 @@
-public class Human {
-    //We set variables that a normal human should have
-    public String name;
-    public int age;
-    public String favoriteAnime;
-    public String burried = "In the ground";
+public abstract class Human {
+    // Encapsulated instance variables
+    private String name;
+    private int age;
+    private String favoriteAnime;
+    private final String buried = "In the ground";
 
+    // Public methods
     public void live() {
         System.out.println("He be livin'");
     }
+
     public void laugh() {
         System.out.println("Ha Ha");
     }
@@ -16,7 +18,32 @@ public class Human {
         System.out.println("Sniff Sniff");
     }
 
-    public String burriedWhere() {
-        return burried;
+    public String getBurialPlace() {
+        return buried;
+    }
+
+    // Getter and Setter methods for instance variables
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFavoriteAnime() {
+        return favoriteAnime;
+    }
+
+    public void setFavoriteAnime(String favoriteAnime) {
+        this.favoriteAnime = favoriteAnime;
     }
 }
--- OOP/LAB1/ClassSystem/src/Main.java
+++ OOP/LAB1/ClassSystem/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Peasant peasant = new Peasant();
         King king = new King();
-        if(Objects.equals(peasant.burriedWhere(), king.burriedWhere())){
+        if(Objects.equals(peasant.getBurialPlace(), king.getBurialPlace())){
             System.out.println("Then who cares.");
         }
     }
