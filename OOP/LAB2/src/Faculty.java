e1b399dc7dab3a4f786a6c65d2110bb865187e39
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-09-24 14:31:24 +0300

    added initial classes

---
+++ OOP/LAB2/src/Faculty.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Faculty {
+    private String name;
+    private String abbreviation;
+    private List<Student> students;
+    private StudyField studyField;
+}
---
+++ OOP/LAB2/src/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date enrollmentDate;
+    private Date dateOfBirth;
+}
