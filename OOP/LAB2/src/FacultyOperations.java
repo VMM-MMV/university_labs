74bf7817afdd6dce57da139996f2e73bece544ed
VMM-MMV <mihai.vieru@isa.utm.md>
2023-09-26 08:59:01 +0300

    doesFacultyExist made general and works for all children of Faculty

--- OOP/LAB2/src/FacultyOperations.java
+++ OOP/LAB2/src/FacultyOperations.java
@@ -2,14 +2,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class FacultyOperations extends Operations {
 
     static void startOperations() {
-        Scanner scanner = new Scanner(System.in);
+        var scanner = new Scanner(System.in);
         String result;
 
         do {
@@ -47,11 +45,11 @@ public class FacultyOperations extends Operations {
     }
 
     private static void newStudent(String commandOperation) {
-        String[] parts = commandOperation.split("/");
+        var parts = commandOperation.split("/");
 
         String facultyAbbreviation = parts[1];
 
-        if (!doesFacultyExist(facultyAbbreviation)) {
+        if (!doesFacultyExist(facultyAbbreviation, studentFaculties)) {
             System.out.println("Faculty Does Not Exist");
             return;
         }
@@ -85,17 +83,28 @@ public class FacultyOperations extends Operations {
     // ns/fcim/mihai/vieru/viem2377@gmail.com/01-09-2022/07-07-2003
 
     private static void graduateStudent(String commandOperation) {
-        System.out.println("graduateStudent");
+        var parts = commandOperation.split("/");
+        String studentEmail = parts[1];
+        for (StudentFaculty faculty : studentFaculties) {
+            for (Student student : faculty.getStudents()) {
+                if (Objects.equals(student.getEmail(), studentEmail)) {
+                    if(!doesFacultyExist(faculty.getAbbreviation(), graduatedFromFaculties)){
+                        GraduatedFromFaculty graduatedFromFaculty = createFacultyForGraduates(faculty);
+                        graduatedFromFaculties.add(graduatedFromFaculty);
+                    }
+                    
+                }
+            }
+        }
     }
 
     private static void displayEnrolled() {
-        for (Faculty faculty : studentFaculties) {
+        for (StudentFaculty faculty : studentFaculties) {
             System.out.println("Faculty: " + faculty.getName());
             for (Student student : faculty.getStudents()) {
                 System.out.println("FirstName: " + student.getFirstName() + " | LastName: " + student.getLastName() + " | Email: " + student.getEmail() + " | DateOfBirth: " + student.getDateOfBirth() + " | EnrollmentDate: " + student.getEnrollmentDate());
             }
         }
-
     }
 
     private static void displayGraduated() {
@@ -106,12 +115,20 @@ public class FacultyOperations extends Operations {
         System.out.println("belongsToFaculty");
     }
 
-    private static boolean doesFacultyExist(String abbreviation) {
-        for (StudentFaculty studentFaculty : studentFaculties) {
-            if (Objects.equals(abbreviation, studentFaculty.getAbbreviation())) {
+    private static boolean doesFacultyExist(String abbreviation, ArrayList<? extends Faculty> faculties) {
+        for (Faculty faculty : faculties) {
+            if (Objects.equals(abbreviation, faculty.getAbbreviation())) {
                 return true;
             }
         }
         return false;
+    }
+
+    private static GraduatedFromFaculty createFacultyForGraduates(StudentFaculty studentFaculty) {
+        String facultyName =  studentFaculty.getName();
+        String facultyAbbreviation =  studentFaculty.getAbbreviation();
+        var facultyStudents =  studentFaculty.getStudents();
+        var facultyStudyField =  studentFaculty.getStudyField();
+        return new GraduatedFromFaculty(facultyName, facultyAbbreviation, facultyStudents, facultyStudyField);
     }
 }
