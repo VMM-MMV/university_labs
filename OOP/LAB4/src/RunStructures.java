93aab9832f6cfc9951dbf45ff2e51eeb2c57ce76
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-08 11:22:56 +0200

    interface for stack implemented

---
+++ OOP/LAB4/src/RunStructures.java
@@ -0,0 +1,5 @@
+public class RunStructures {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOP/LAB4/src/Stack.java
@@ -0,0 +1,6 @@
+public interface Stack<E> {
+    void push();
+    E pop();
+    E peek();
+    boolean isNull();
+}
