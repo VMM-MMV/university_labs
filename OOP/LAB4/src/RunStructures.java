cefa8bdc4253f65a76999f8f177cfb58f2df3176
VMM-MMV <mihai.vieru@isa.utm.md>
2023-11-17 21:07:33 +0200

    implemented QueueBuffer

--- OOP/LAB4/src/RunStructures.java
+++ OOP/LAB4/src/RunStructures.java
@@ -10,9 +10,26 @@ public class RunStructures {
 //        stackList.push("Vora");
 //        stackList.push(1);
 //        System.out.println(stackList.toStrings());
-        StackArray<Object> stackArray = new StackArray<>();
-        stackArray.push(5);
-        stackArray.push("ahahah");
-        System.out.println(stackArray.toStrings());
+//        StackArray<Object> stackArray = new StackArray<>();
+//        stackArray.push(5);
+//        stackArray.push("ahahah");
+//        System.out.println(stackArray.toStrings());
+        QueueBuffer<Object> queueBuffer = new QueueBuffer<>(5);
+        queueBuffer.enqueue(1);
+        queueBuffer.enqueue(2);
+        queueBuffer.enqueue(3);
+        queueBuffer.enqueue(4);
+        queueBuffer.enqueue(5);
+        queueBuffer.dequeue();
+        queueBuffer.dequeue();
+//        queueBuffer.dequeue();
+//        queueBuffer.dequeue();
+//        queueBuffer.dequeue();
+        queueBuffer.enqueue(6);
+        queueBuffer.enqueue(7);
+        System.out.println(queueBuffer.toStrings());
+        System.out.println(queueBuffer.peek());
+//        System.out.println(queueArray.head);
+//        System.out.println(queueArray.tail);
     }
 }
