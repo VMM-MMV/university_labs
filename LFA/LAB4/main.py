7996cacacffea00057301ae066efa8eb7e5b01d2
vmm-mmv <mihai.vieru@isa.utm.md>
2024-04-08 11:16:37 +0300

    added power handling

--- LFA/LAB4/main.py
+++ LFA/LAB4/main.py
@@ -1,7 +1,6 @@
-string1 = "(SML|T|A)+(U|V)w*y+24"
+string1 = "bA^3b"#"(SML|T|A)+(U|V)w*y+24"
 string2 = "L(M|N)D^3p*Q(2|3)"
 string3 = "R*S(T|U|V)w(x|y|z)^2"
-
 
 change_string = string1
 grammar = ""
@@ -21,6 +20,7 @@ class RegexGrammar():
         self.data = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.mappedReplacements = {}
         self.replaceString()
+        self.handlePower()
         self.iterator = Iterator(self.data)
         self.current_non_terminal = next(self.iterator)
         self.previous_non_terminal = ""
@@ -32,7 +32,8 @@ class RegexGrammar():
         for char_id in range(len(self.string)):
             char = self.string[char_id]
             if char.isalpha():
-                self.data.remove(char.upper())  # Remove used terminals
+                if char.upper() in self.data:
+                    self.data.remove(char.upper())  # Remove used terminals
             
             if char == char.upper() and char.isalpha():
                 self.mappedReplacements[char.lower()] = char # Replace and add the terminals to a map
@@ -48,6 +49,34 @@ class RegexGrammar():
             if not char.isalnum():
                 power_simbol = char
         print(self.mappedReplacements)
+
+    def handlePower(self):
+        char_id = 0
+        while char_id < len(self.string):
+            char = self.string[char_id]
+            token = ""
+            start_pos = char_id
+            if char == "(":
+                while char != ")" and char_id < len(self.string):
+                    char = self.string[char_id]
+                    token += char
+                    char_id += 1
+            else:
+                token += char
+                char_id += 1
+
+            if char_id < len(self.string):
+                if self.string[char_id] == "^":
+                    char_id += 1
+                    number_token = ""
+                    while char_id < len(self.string):
+                        char = self.string[char_id]
+                        if not char.isdigit():
+                            break
+                        number_token += self.string[char_id]
+                        char_id += 1
+                    self.string = self.string[:start_pos] + str(token)*int(number_token) + self.string[char_id:]
+                    print(self.string)
 
     def add_state(self):
         new_non_terminal = next(self.iterator)
@@ -105,7 +134,7 @@ class RegexGrammar():
             self.coursor += 1
         print(self.grammar)        
 
-regex = RegexGrammar(string1)
+regex = RegexGrammar(string2)
 
 regex.handleGrammar()
 
