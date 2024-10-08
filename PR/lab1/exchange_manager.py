a0ee301b3047d06c44b71531fa5982f1d11d7cf3
vmm-mmv <mihai.vieru@isa.utm.md>
2024-10-08 22:25:53 +0300

    added validation for price and reviews

--- PR/lab1/exchange_manager.py
+++ PR/lab1/exchange_manager.py
@@ -10,8 +10,8 @@ def get_new_exchange_rate():
 
     return float(soup.find('strong').text.split("=")[1].strip().split(" ")[0].replace(",","."))
 
-def is_older_than_24_hours(stored_date):
-    # Function to check if date is older than 24 hours
+def is_older_than_24_hours(stored_date_str):
+    stored_date = datetime.strptime(stored_date_str, "%Y-%m-%d %H:%M:%S")
     current_time = datetime.now()
     time_difference = current_time - stored_date
     return time_difference > timedelta(hours=24)
@@ -22,11 +22,8 @@ def get_exchange_rate(currency_type):
     exchange_records = 'exchange_rates.json'
     json_data = read_json(exchange_records)
 
-    stored_date_str = json_data.get("date")
-    stored_date = datetime.strptime(stored_date_str, "%Y-%m-%d %H:%M:%S")
-
     # Update if date is older than 24 hours
-    if is_older_than_24_hours(stored_date):
+    if is_older_than_24_hours(json_data.get("date")):
         json_data["date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         json_data[currency_type] = get_exchange_rate()
         write_json(exchange_records, json_data)
--- PR/lab1/main.py
+++ PR/lab1/main.py
@@ -2,6 +2,25 @@ from bs4 import BeautifulSoup
 from http_sender import get_html_content
 import time
 
+def get_reviews(link):
+    try:
+        game_page_html = get_html_content(link)
+        soup = BeautifulSoup(game_page_html, 'html.parser')
+        reviews = soup.find("span", class_="game_review_summary").text
+
+        # if a review has a int, it does not have enough votes to be reviewed
+        review_has_int = any(char.isdigit() for char in reviews)
+        if review_has_int: return "Not Enough"
+        return reviews
+    except:
+        return None
+
+def get_processed_price(full_price):
+    if "free" in full_price.lower(): return 0
+
+    full_price = full_price.split("$")[1]
+    return float(full_price)
+
 url = 'https://store.steampowered.com/explore/new/'
 
 html_content = get_html_content(url)
@@ -10,16 +29,12 @@ soup = BeautifulSoup(html_content, 'html.parser')
 games = soup.find_all('a', class_='tab_item')
 
 for game in games:
-    price = game.find('div', class_="discount_final_price").text
+    full_price = game.find('div', class_="discount_final_price").text
+    price = get_processed_price(full_price)
+    print(price)
+
     name = game.find('div', class_="tab_item_name").text
     link = game['href']
-    try:
-        game_page_html = get_html_content(link)
-        soup = BeautifulSoup(game_page_html, 'html.parser')
-        reviews = soup.find("span", class_="game_review_summary").text
-        if any(char.isdigit() for char in reviews):
-            reviews = "Not Enough"
-    except:
-        reviews = None
-    print(reviews)
-    # time.sleep(1)
+    
+    reviews = get_reviews(link)
+    time.sleep(0.5)
