110a7008877598437e707319a028416a84d86d94
vmm-mmv <mihai.vieru@isa.utm.md>
2024-10-09 21:45:03 +0300

    added prettier json

--- PR/lab1/json_parser.py
+++ PR/lab1/json_parser.py
@@ -7,32 +7,40 @@ def getWI(text, indent, new_line=True):
     else:
         return indented_text
 
-def encode(node, indent=4, indent_level=4):
+def encode(node, indent=4, indent_level=0):
     json_str = ""
     if isinstance(node, dict):
-        json_str += getWI("{", indent - indent_level)
+        json_str += getWI("{", indent_level)
         
         for key, value in node.items():
-            json_str += getWI(f'"{key}": ', indent)
-            json_str += encode(value, indent) + ","
+            json_str += getWI(f'"{key}": ', indent_level + indent)
+            if isinstance(value, (dict, list)):
+                json_str += encode(value, indent, indent_level + indent)
+            else:
+                json_str += encode(value, indent, 0)
+            json_str += ","
         
-        json_str = json_str[:len(json_str)-1]
-        json_str += getWI("}", indent - indent_level)
+        if json_str.endswith(","):
+            json_str = json_str[:-1]
+
+        json_str += getWI("}", indent_level)
         return json_str
     
     elif isinstance(node, list):
-        json_str += getWI("[", indent - indent_level, False)
+        json_str += getWI("[", indent_level)
         
         for child_node in node:
-            json_str += encode(child_node, indent + indent_level) + ","
+            json_str += encode(child_node, indent, indent_level + indent) + ","
         
-        json_str = json_str[:len(json_str)-1]
-        json_str += getWI("]", indent - indent_level)
+        if json_str.endswith(","):
+            json_str = json_str[:-1]
+            
+        json_str += getWI("]", indent_level)
         return json_str
     
     else:
-        return getWI(f'"{node}"', indent, False)
-
+        return f'"{node}"'
+    
 if __name__ == "__main__":
     data = dict(read_json("data.json"))
     res = encode(data)
