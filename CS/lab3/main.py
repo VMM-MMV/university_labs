10f6a14e0ddbe099c28e4b1945b50962d7c96f49
vmm-mmv <mihai.vieru@isa.utm.md>
2024-10-27 12:49:03 +0200

    added decription

--- CS/lab3/main.py
+++ CS/lab3/main.py
@@ -1,16 +1,14 @@
 romanian_alphabet = "AĂÂBCDEFGHIÎJKLMNOPQRSȘTȚUVWXYZ"
 
-message = "Salut, ce mai faci în această zi frumoasă?"
+message = "Salutcemaifaciînaceastăzifrumoasă"
 key = "afară"
 
-import re
-
-def replace_non_alpha(text):
-    return re.sub(r'[^A-Za-z]+', '', text)
+def format_string(message):
+    return "".join(message.split()).upper()
 
 def yield_encrypt(message, key, alphabet=romanian_alphabet):
-    message = replace_non_alpha(message.upper())
-    key = key.upper()
+    message = format_string(message)
+    key = format_string(key)
     for i in range(len(message)):
         key_i = i % len(key)
         m_num = alphabet.find(message[i])
@@ -18,10 +16,24 @@ def yield_encrypt(message, key, alphabet=romanian_alphabet):
         enc_char = (m_num + k_num) % len(alphabet)
         yield alphabet[enc_char]
         
+def yield_decrypt(message, key, alphabet=romanian_alphabet):
+    message = format_string(message)
+    key = format_string(key)
+    for i in range(len(message)):
+        key_i = i % len(key)
+        m_num = alphabet.find(message[i])
+        k_num = alphabet.find(key[key_i])
+        enc_char = (m_num - k_num) % len(alphabet)
+        yield alphabet[enc_char]
+        
 def encrypt(message, key, alphabet=romanian_alphabet):
     return "".join((yield_encrypt(message, key, alphabet)))
 
-res = encrypt(message, key)
+def decrypt(enc_message, key, alphabet=romanian_alphabet):
+    return "".join((yield_decrypt(enc_message, key, alphabet)))
 
-# romanian_alphabet.find()
-print(res)
+enc_message = encrypt(message, key)
+
+message = decrypt(enc_message, key)
+
+print(enc_message, message)
