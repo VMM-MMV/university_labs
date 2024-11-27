5462e16f1291d80e98a0413dc1dd12de516e9b37
vmm-mmv <mihai.vieru@isa.utm.md>
2024-11-27 23:52:59 +0200

    added consumer

---
+++ PR/lab3/src/main/java/crazy/RabbitMQConsumer.java
@@ -0,0 +1,13 @@
+package crazy;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConsumer {
+
+    @RabbitListener(queues = "queue")
+    public void receiveMessage(String message) {
+        System.out.println("Received: " + message);
+    }
+}
