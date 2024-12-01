f5bff8f6a506f08c17eb3d0c3ee41fda1c6c4612
vmm-mmv <mihai.vieru@isa.utm.md>
2024-12-01 15:37:44 +0200

    added left shidts

--- CS/lab4/main.py
+++ CS/lab4/main.py
@@ -1,3 +1,13 @@
+def get_binary(text):
+    bytes_data = text.encode("utf-8")
+    binary_representation = "".join([format(byte, '08b') for byte in bytes_data])
+    return binary_representation
+
+def print_b(binary_string):
+    # Ensure the binary string is padded to a multiple of 8
+    padded_binary = binary_string.ljust((len(binary_string) + 7) // 8 * 8, '0')
+    spaced_binary = ' '.join(padded_binary[i:i+8] for i in range(0, len(padded_binary), 8))
+    print(spaced_binary)
 
 PC1 = [  # Permuted Choice 1
     57, 49, 41, 33, 25, 17,  9,
@@ -10,25 +20,25 @@ PC1 = [  # Permuted Choice 1
     21, 13,  5, 28, 20, 12,  4
 ]
 
-def get_binary(text):
-    bytes_data = text.encode("utf-8")
-    binary_representation = "".join([format(byte, '08b') for byte in bytes_data])
-    return binary_representation
+BIT_SHIFTS = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
-def print_b(binary_string):
-    spaced_binary = ' '.join(binary_string[i:i+8] for i in range(0, len(binary_string), 8))
-    print(spaced_binary)
+def apply_pc1(input_binary):
+    return ''.join(input_binary[i-1] for i in PC1)
 
-def apply_pc1(input_bytes):
-    return "".join(input_bytes[i-1] for i in PC1)
-
-
+def apply_left_bit_shifts(input_binary):
+    shifted_keys = []
+    for shift in BIT_SHIFTS:
+        input_binary = input_binary[shift:] + input_binary[:shift]
+        shifted_keys.append(input_binary)
+    return shifted_keys
 
 message = "Hello"
 key = "crazy ass key man stuff is crazy"
 
 b_key = get_binary(key)
-b_key = b_key[:64]
+
+b_key = b_key[:64].ljust(64, '0')
+
 print_b(b_key)
 
 K_plus = apply_pc1(b_key)
@@ -36,10 +46,17 @@ print_b(K_plus)
 
 C0 = K_plus[:28]
 D0 = K_plus[28:]
+
 print_b(C0)
 print_b(D0)
 
+C = apply_left_bit_shifts(C0)
+D = apply_left_bit_shifts(D0)
 
+for i, c in enumerate(C, 1):
+    print(f"C{i}:", end=" ")
+    print_b(c)
 
-
-
+for i, d in enumerate(D, 1):
+    print(f"D{i}:", end=" ")
+    print_b(d)
