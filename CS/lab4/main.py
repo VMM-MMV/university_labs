04157fe96ed849162d40c57762f4c2e514551118
vmm-mmv <mihai.vieru@isa.utm.md>
2024-12-01 16:09:56 +0200

    added ip table

--- CS/lab4/main.py
+++ CS/lab4/main.py
@@ -31,6 +31,17 @@ PC2 = [  # Permuted Choice 2 48
     25, 19,  4, 15, 22,  9, 27, 13
 ]
 
+IP = [   # Initial Permutation
+    58, 50, 42, 34, 26, 18, 10,  2,
+    60, 52, 44, 36, 28, 20, 12,  4,
+    62, 54, 46, 38, 30, 22, 14,  6,
+    64, 56, 48, 40, 32, 24, 16,  8,
+    57, 49, 41, 33, 25, 17,  9,  1,
+    59, 51, 43, 35, 27, 19, 11,  3,
+    61, 53, 45, 37, 29, 21, 13,  5,
+    63, 55, 47, 39, 31, 23, 15,  7
+]
+
 def apply_table(input_binary, table):
     return ''.join(input_binary[i-1] for i in table)
 
@@ -78,4 +89,13 @@ def get_keys(key):
 
 message = "Hello"
 key = "crazy ass key man stuff is crazy"
-get_keys(key)
+# get_keys(key)
+
+b_message = get_binary(message)
+b_message = b_message[:64].ljust(64, '0')
+print_b(b_message)
+
+permuted_message = apply_table(b_message, IP)
+print_b(permuted_message)
+
+
