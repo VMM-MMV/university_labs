03da592f4fd78bf8a0e3e9ba007bc3dcf7893744
vmm-mmv <mihai.vieru@isa.utm.md>
2024-12-07 15:25:48 +0200

    added postgre in docker

--- PR/lab3/raft/src/main/java/com/example/NodeController.java
+++ PR/lab3/raft/src/main/java/com/example/NodeController.java
@@ -3,12 +3,14 @@ package com.example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/raft/api/v1")
 public class NodeController {
 
     private final NodeService nodeService;
