211c383ca7e06e67f846adbae0c920e4f0786df8
vmm-mmv <mihai.vieru@isa.utm.md>
2025-02-26 13:38:57 -0800

    improved main with new abstractions

--- ES/lab1/src/main.cpp
+++ ES/lab1/src/main.cpp
@@ -5,81 +5,46 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Keypad.h>
+#include "KeypadWrapper.h"
+#include "LCD.h"
+#include "KLRedirector.h"
 
-const byte ROWS = 4; 
-const byte COLS = 4; 
+Led greenLed(13);
+Led redLed(12);
 
-char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+KeypadWrapper keypad;
+LCD lcd;
+KLRedirector klRedirector(lcd, keypad);
 
-byte rowPins[ROWS] = {4, 5, 6, 7};
-byte colPins[COLS] = {8, 9, 10, 11};
-
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-
-Led ledVerde = Led(13);
-Led ledRosu = Led(12);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-const String codCorect = "1234";  
-String codIntroduc = "";  
+const char* rightCode = "1234";
 
 void setup() {
   Serial.begin(9600);
-  delay(500);
-
+  
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
-
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Introduceti cod:");
+  klRedirector.init();
 }
 
 void loop() {
-  char customKey = customKeypad.getKey();
+  char input[5] = {0};
   
-  if (customKey) {
-    lcd.setCursor(0, 1);
-    
-    if (customKey == '#') {
-      if (codIntroduc == codCorect) {
-        lcd.print("Cod valid");
-        ledVerde.on();
-        ledRosu.off();
-      } else {
-        lcd.print("Cod invalid");
-        ledRosu.on();
-        ledVerde.off();
-      }
-      delay(2000);
-      codIntroduc = "";
-      lcd.setCursor(0, 0);
-      lcd.print("Introduceti cod:");
-    } else if (customKey == '*') {
-      codIntroduc = "";
-      lcd.setCursor(0, 1);
-      lcd.print("                ");
-    } else {
-      codIntroduc += customKey;
-      lcd.print(customKey);
-    }
+  lcd.clear();
+  printf("Enter code: ");
+  
+  scanf("%4s", input);
+  
+  lcd.clear();
+  
+  if (strcmp(input, rightCode) == 0) {
+    printf("Code Valid!");
+    greenLed.on();
+    redLed.off();
+  } else {
+    printf("Code Invalid!");
+    redLed.on();
+    greenLed.off();
   }
+
+  delay(2000);
 }
-
-// void lab1() {
-//   printf("Enter your command: ");
-//   char* input = IO::input();
-
-//   if (strcmp(input, "led on") == 0) {
-//       led.on();
-//   } else if (strcmp(input, "led off") == 0) {
-//       led.off();
-//   }
-//   printf("\n");
-// }
