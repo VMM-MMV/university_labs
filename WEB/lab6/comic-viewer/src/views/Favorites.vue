368d2c5c3c92b6f30df53305a04eae44f6cd9b9e
vmm-mmv <mihai.vieru@isa.utm.md>
2025-05-12 06:42:11 -0700

    refactor favorites color

--- WEB/lab6/comic-viewer/src/views/Favorites.vue
+++ WEB/lab6/comic-viewer/src/views/Favorites.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-bold mb-6">My Favorites</h1>
+    <h1 class="text-3xl text-secondary font-bold mb-6">My Favorites</h1>
 
     <!-- Empty state using component -->
     <EmptyState 
